7dc432ea984fbaa7a8ec1fa72296b73525df0d7e
Damian Czajkowski <d0.czajkowski@gmail.com>
2024-04-09 15:54:04 +0200

    wrap-attributes-names-in-__attrname__-in-error-messages

--- tests_next/snapshots/snap_test_union_type_validation.py
+++ tests_next/snapshots/snap_test_union_type_validation.py
@@ -7,6 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_missing_type_in_schema 1'] = "Types {'Comment'} are defined in __types__ but not present in the __schema__."
+snapshots['test_missing_type_in_schema 1'] = "Types __Comment__ are defined in __types__ but not present in the __schema__."
 
-snapshots['test_missing_type_in_types 1'] = "Types {'Comment'} are present in the __schema__ but not defined in __types__."
+snapshots['test_missing_type_in_types 1'] = "Types __Comment__ are present in the __schema__ but not defined in __types__."
